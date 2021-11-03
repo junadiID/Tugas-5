af9cd18aa355616d005b9ddb1ee2e42b8cb168f5
Junjun Junaedi <79783334+junadiID@users.noreply.github.com>
2021-11-03 19:30:51 +0700

    Update Tugas_No1_Junjun.js

--- Tugas_No1_Junjun.js
+++ Tugas_No1_Junjun.js
@@ -1,3 +1,3 @@
-var Fruit = ["Apel","Pisang","Jambu","Anggur","Pepaya","Duku","Durian","Jeruk","Mangga","Manggis"];
-Fruit.sort(); // mengerutkan element Fruit
-console.log (Fruit);
+var fruits = ["Apel","Pisang","Jambu","Anggur","Pepaya","Duku","Durian","Jeruk","Mangga","Manggis"]
+var  newArray = fruits.sort();
+console.log(newArray); 
